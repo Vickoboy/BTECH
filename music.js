b520dbe9d3bb36104e7e256d4407f305eb3c57cc
Vickoboy <vickoboy104@gmail.com>
2023-11-25 10:47:28 +0100

    btech website

---
+++ music.js
@@ -0,0 +1,144 @@
+let now_playing = document.querySelector(".now-playing");
+let track_art = document.querySelector(".track-art");
+let track_name = document.querySelector(".track-name");
+let track_artist = document.querySelector(".track-artist");
+
+let playpause_btn = document.querySelector(".playpause-track");
+let next_btn = document.querySelector(".next-track");
+let prev_btn = document.querySelector(".prev-track");
+
+let seek_slider = document.querySelector(".seek_slider");
+let volume_slider = document.querySelector(".volume_slider");
+let curr_time = document.querySelector(".current-time");
+let total_duration = document.querySelector(".total-duration");
+
+let track_index = 0;
+let isPlaying = false;
+let updateTimer;
+
+// Create new audio element
+let curr_track = document.createElement('audio');
+
+// Define the tracks that have to be played
+let track_list = [
+  {
+    name: "All I Need Is You",
+    artist: "Hillsong",
+    image: "images/music/all-i-need-is-you.png",
+    path: "audio/music/ALL-I-NEED-IS-YOU-LIVE-IN-MIAMI-Hillsong-UNITED.mp3",
+  },
+  {
+    name: "Put Your Name On Me",
+    artist: "Put-Your-Name On-Me-(feat-Tony-Fresh)",
+    image: "images/music/Put Your Name On Me (feat. Tony Fresh).png",
+    path: "audio/music/Put_Your_Name_On_Me_(feat._Tony_Fresh)__Official_Music_Video(256k).mp3",
+  },
+  {
+    name: "See A Victory",
+    artist: "Elevation Worship",
+    image: "images/music/see-a-victory.png",
+    path: "audio/music/See_A_Victory_feat._Travis_Greene__Live_From_Elevation_Ballantyne__Elevation_Worship(256k).mp3",
+  },
+  {
+    name: "IBA",
+    artist: "Nathaniel BAsey feat Dunsin Oyekan & Dasola Akinbule",
+    image: "images/music/iba.png",
+    path: "audio/music/IBA__NATHANIEL_BASSEY_feat._DUNSIN_OYEKAN_&_DASOLA_AKINBULE_#nathanielbassey_#dunsinoyekan_#iba(256k).mp3",
+  },
+  {
+    name: "Refuge",
+    artist: "New Creation Worship",
+    image: "images/music/refuge.png",
+    path: "audio/music/Refuge__New_Creation_Worship(256k).mp3",
+  },
+];
+
+function loadTrack(track_index) {
+  clearInterval(updateTimer);
+  resetValues();
+  curr_track.src = track_list[track_index].path;
+  curr_track.load();
+
+  track_art.style.backgroundImage = "url(" + track_list[track_index].image + ")";
+  track_name.textContent = track_list[track_index].name;
+  track_artist.textContent = track_list[track_index].artist;
+  now_playing.textContent = "PLAYING " + (track_index + 1) + " OF " + track_list.length;
+
+  updateTimer = setInterval(seekUpdate, 1000);
+  curr_track.addEventListener("ended", nextTrack);
+}
+
+function resetValues() {
+  curr_time.textContent = "00:00";
+  total_duration.textContent = "00:00";
+  seek_slider.value = 0;
+}
+
+// Load the first track in the tracklist
+loadTrack(track_index);
+
+function playpauseTrack() {
+  if (!isPlaying) playTrack();
+  else pauseTrack();
+}
+
+function playTrack() {
+  curr_track.play();
+  isPlaying = true;
+  playpause_btn.innerHTML = '<i class="fa fa-pause-circle fa-5x"></i>';
+}
+
+function pauseTrack() {
+  curr_track.pause();
+  isPlaying = false;
+  playpause_btn.innerHTML = '<i class="fa fa-play-circle fa-5x"></i>';;
+}
+
+function nextTrack() {
+  if (track_index < track_list.length - 1)
+    track_index += 1;
+  else track_index = 0;
+  loadTrack(track_index);
+  playTrack();
+}
+
+function prevTrack() {
+  if (track_index > 0)
+    track_index -= 1;
+  else track_index = track_list.length;
+  loadTrack(track_index);
+  playTrack();
+}
+
+function seekTo() {
+  let seekto = curr_track.duration * (seek_slider.value / 100);
+  curr_track.currentTime = seekto;
+}
+
+function setVolume() {
+  curr_track.volume = volume_slider.value / 100;
+}
+
+function seekUpdate() {
+  let seekPosition = 0;
+
+  if (!isNaN(curr_track.duration)) {
+    seekPosition = curr_track.currentTime * (100 / curr_track.duration);
+
+    seek_slider.value = seekPosition;
+
+    let currentMinutes = Math.floor(curr_track.currentTime / 60);
+    let currentSeconds = Math.floor(curr_track.currentTime - currentMinutes * 60);
+    let durationMinutes = Math.floor(curr_track.duration / 60);
+    let durationSeconds = Math.floor(curr_track.duration - durationMinutes * 60);
+
+    if (currentSeconds < 10) { currentSeconds = "0" + currentSeconds; }
+    if (durationSeconds < 10) { durationSeconds = "0" + durationSeconds; }
+    if (currentMinutes < 10) { currentMinutes = "0" + currentMinutes; }
+    if (durationMinutes < 10) { durationMinutes = "0" + durationMinutes; }
+
+    curr_time.textContent = currentMinutes + ":" + currentSeconds;
+    total_duration.textContent = durationMinutes + ":" + durationSeconds;
+  }
+}
+
